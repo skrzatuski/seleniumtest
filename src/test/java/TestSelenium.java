a4fa796de6247f4cebb8f8d98648e23725539617
skrzatuski <skrzatuski01@gmail.com>
2022-05-11 16:12:02 +0200

    kkk

--- src/test/java/TestSelenium.java
+++ src/test/java/TestSelenium.java
@@ -10,7 +10,6 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.IOException;
 import java.io.File;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
